d0fd8b470b89c02dc03820148ff05242a78ec3d7
rlzmtnck <rlzmtnck@student.telkomuniversity.ac.id>
2022-08-14 22:31:17 +0700

    ready

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 
-
+const dotenv = require('dotenv');
+dotenv.config( { path : 'config.env'} )
 
 $("#add_user").submit(function(event){
     alert("Data Inserted Successfully!");
@@ -17,7 +18,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `http://localhost:5000/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -34,7 +35,7 @@ if(window.location.pathname == "/list-user"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `http://localhost:5000/api/users/${id}`,
             "method" : "DELETE"
         }
 
